74b7f71a0f3bb23cb534830fe87033e5c0bbf649
Alex McKenzie <alex@unbreak-it.com>
2022-01-10 23:47:51 -0500

    update converter to return undefined

--- converter.ts
+++ converter.ts
@@ -24,7 +24,10 @@ export class Converter {
 	 * @see Converter.marshall Converter.marshall to
 	 *    convert entire records (rather than individual attributes)
 	 */
-	static input(data: any, options?: ConverterOptions): AttributeValue {
+	static input(
+		data: any,
+		options?: ConverterOptions,
+	): AttributeValue | undefined {
 		options = options || {};
 		const type = typeOf(data);
 		if (type === 'Date') {
@@ -56,9 +59,6 @@ export class Converter {
 			// this value has a custom constructor
 			return formatMap(data, options);
 		}
-		return {
-			$unknown: [type, data],
-		};
 	}
 
 	/**
